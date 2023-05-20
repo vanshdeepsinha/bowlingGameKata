2550dc1d02a6911d098c8f721958b03c08fefa55
vanshdeep <vanshdeepsinha@gmail.com>
2023-05-20 16:49:05 +0530

    Initial set up

---
+++ src/main/java/com/bowlinggame/App.java
@@ -0,0 +1,14 @@
+package com.bowlinggame;
+/**
+ * 
+ * @author Vanshdeep
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
