3fdc022696373498ec7a7c8c08ee78474bcb809b
vanshdeep <vanshdeepsinha@gmail.com>
2023-05-24 12:35:03 +0530

    test: integrated spare functionality to get the score but failed

--- src/main/java/com/bowlinggame/BowlingGame.java
+++ src/main/java/com/bowlinggame/BowlingGame.java
@@ -2,15 +2,25 @@ package com.bowlinggame;
 
 public class BowlingGame {
 	
-	private int score = 0;
+	private int roll =0;
+	private int[] rolls = new int[21];
 
 	public void rollBall(int pinsDown) {
 		
-		score += pinsDown;
+		rolls[roll++]= pinsDown;
 	
 	}
 
 	public int getScore() {
+		
+		int score = 0;
+		int cursor =0;
+		
+		for (int frames = 0; frames < 10; frames++) {
+			 
+			 score += rolls[cursor] + rolls[cursor+1];
+			 cursor +=2;
+		}
 
 		return score;
 	}
