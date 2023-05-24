b88345b05a0a6c16a150af1e34df3f3c3eac1335
vanshdeep <vanshdeepsinha@gmail.com>
2023-05-24 12:40:41 +0530

    feat: implemented feature to score spare

--- src/main/java/com/bowlinggame/BowlingGame.java
+++ src/main/java/com/bowlinggame/BowlingGame.java
@@ -17,9 +17,14 @@ public class BowlingGame {
 		int cursor =0;
 		
 		for (int frames = 0; frames < 10; frames++) {
-			 
-			 score += rolls[cursor] + rolls[cursor+1];
-			 cursor +=2;
+			  if(rolls[cursor]+rolls[cursor+1] == 10) {
+				score += 10 + rolls[cursor+2];
+				cursor +=2;
+			  } else {
+				  score += rolls[cursor] + rolls[cursor+1];
+				  cursor +=2;
+			  }
+			 	
 		}
 
 		return score;
